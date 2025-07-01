8ecc321c76c576a3af09119066a5d985a718e2f6
Karim Kilimoh <karimmfaume10@gmail.com>
2025-07-01 21:03:26 +0300

    Personal link on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -77,7 +77,7 @@ function Footer() {
       {/* Copyright */}
       <div className="footer-copyright">
         &copy; {new Date().getFullYear()} Tanzanian Diaspora in Egypt. All rights reserved.
-        <p href="https://www.linkedin.com/in/karim-kilimoh-b6b733127/" target="_blank" rel="noopener noreferrer">Developed by Karim Kilimoh</p>
+        <p>Developed by <a href="https://www.linkedin.com/in/karim-kilimoh-b6b733127/" target="_blank" rel="noopener noreferrer"> Karim Kilimoh</a></p>
       </div>
     </footer>
   );
